d86babbfe2bfdef880225276346ff2df5846ef71
yopaseopor <yopaseopor@gmail.com>
2021-12-08 22:10:07 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -431,7 +431,44 @@ var config = {
 
 				var stroke = new ol.style.Stroke({
 					color: '#0000ff',
-					width: 1.25
+					width: 0.5
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Circle({
+						fill: fill,
+						stroke: stroke,
+						radius: 1
+					}),
+							text: new ol.style.Text({
+								color: 'white',
+								text: name,
+								font: '14px Verdana',
+								offsetX : 0,
+								offsetY : 12
+							}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+		},
+		{
+			group: 'Name:ca',
+			title: 'Name:ca via',
+			query: '(way["name:ca"]["name"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:#0000ff',
+			style: function (feature) {
+				var key_regex = /^name:ca$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(0,0,255,0.1)'
+				});
+
+				var stroke = new ol.style.Stroke({
+					color: '#0000ff',
+					width: 0.5
 				});
 				var style = new ol.style.Style({
 					image: new ol.style.Circle({
@@ -442,9 +479,8 @@ var config = {
 							text: new ol.style.Text({
 								color: 'white',
 								text: name,
-								font: '14px Verdana',
-								offsetX : 0,
-								offsetY : 12
+								placement:'line',
+								font: '14px Verdana'
 							}),
 					fill: fill,
 					stroke: stroke
